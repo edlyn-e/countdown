fd8d36ec455d55badcaeef4df966db88e23d2c81
Edlyn Evangelista <107823538+edlyn-e@users.noreply.github.com>
2022-06-30 14:51:50 +0930

    fixed minute format

--- countdown.js
+++ countdown.js
@@ -16,7 +16,7 @@ const months = [
 
 const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-const endDate = document.querySelector("#endDate");
+const endDate = document.querySelector(".endDate");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
@@ -26,7 +26,7 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
 // Enter new dates here:
-let futureDate = new Date(2023, 2, 11, 9, 30);
+let futureDate = new Date(2022, 11, 18, 9, 0);
 // Date format (Year, Month, Day, Hour, Min)
 // The year and the month are on a 0-start index basis.
 // Month: 0 = Jan, 1 = Feb, 2 = Mar, etc.
@@ -37,11 +37,12 @@ let futureDate = new Date(2023, 2, 11, 9, 30);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
+const minutes = `${futureDate.getMinutes()}`.padStart(2, 0);
+// padStart(max length of characters, character to be inserted at the start)
 
 let month = futureDate.getMonth();
 month = months[month];
-// access months from arrays
+// access month from arrays
 
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
